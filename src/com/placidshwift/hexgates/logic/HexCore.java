1b634bc02df815fdd6d233ed21794e5afac8d852
James Ybanez <ybanezjame@myvuw.ac.nz>
2021-12-28 11:10:39 +1300

    Structure generation

--- src/com/placidshwift/hexgates/logic/HexCore.java
+++ src/com/placidshwift/hexgates/logic/HexCore.java
@@ -44,6 +44,15 @@ public class HexCore {
 			}
 		}
 		
+		for (int x = 1; x < 5; x++) {
+			world.getBlockAt(loc.getBlockX()-2, loc.getBlockY()-1+x, loc.getBlockZ()-2).setType(Material.COBBLED_DEEPSLATE_WALL);
+			world.getBlockAt(loc.getBlockX()+2, loc.getBlockY()-1+x, loc.getBlockZ()-2).setType(Material.COBBLED_DEEPSLATE_WALL);
+			world.getBlockAt(loc.getBlockX()-2, loc.getBlockY()-1+x, loc.getBlockZ()+2).setType(Material.COBBLED_DEEPSLATE_WALL);
+			world.getBlockAt(loc.getBlockX()+2, loc.getBlockY()-1+x, loc.getBlockZ()+2).setType(Material.COBBLED_DEEPSLATE_WALL);
+			
+		}
+		
+		
 		
 		// Layer 1
 		world.getBlockAt(loc.getBlockX(), loc.getBlockY()-1, loc.getBlockZ()).setType(Material.IRON_BLOCK);
@@ -202,10 +211,89 @@ public class HexCore {
 
 		
 		// North Layer 3 Stairs
-		world.getBlockAt(loc.getBlockX(), loc.getBlockY()-3, loc.getBlockZ()-4).setType(Material.STONE_BRICK_STAIRS);  // Front Midlee
+		world.getBlockAt(loc.getBlockX(), loc.getBlockY()-3, loc.getBlockZ()-4).setType(Material.STONE_BRICK_STAIRS);  // Front Middle
 		block = world.getBlockAt(loc.getBlockX(), loc.getBlockY()-3, loc.getBlockZ()-4);
 		direction = (Directional) block.getBlockData();
 		direction.setFacing(BlockFace.SOUTH);
 		block.setBlockData(direction);
+		
+		// South Layer 2
+		world.getBlockAt(loc.getBlockX()+1, loc.getBlockY()-2, loc.getBlockZ()+3).setType(Material.POLISHED_ANDESITE); // LEFT
+		world.getBlockAt(loc.getBlockX()-1, loc.getBlockY()-2, loc.getBlockZ()+3).setType(Material.POLISHED_ANDESITE); // RIGHT
+		
+		// South Layer 2 Stairs
+		world.getBlockAt(loc.getBlockX(), loc.getBlockY()-2, loc.getBlockZ()+3).setType(Material.STONE_BRICK_STAIRS);  // Middle
+		block = world.getBlockAt(loc.getBlockX(), loc.getBlockY()-2, loc.getBlockZ()+3);
+		direction = (Directional) block.getBlockData();
+		direction.setFacing(BlockFace.NORTH);
+		block.setBlockData(direction);
+		
+		// South Layer 3
+		
+		world.getBlockAt(loc.getBlockX(), loc.getBlockY()-3, loc.getBlockZ()+3).setType(Material.POLISHED_ANDESITE); // Middle
+		
+		world.getBlockAt(loc.getBlockX()+1, loc.getBlockY()-3, loc.getBlockZ()+3).setType(Material.POLISHED_ANDESITE); // LEFT
+		world.getBlockAt(loc.getBlockX()+1, loc.getBlockY()-3, loc.getBlockZ()+4).setType(Material.POLISHED_ANDESITE); // FRONT LEFT
+		
+		world.getBlockAt(loc.getBlockX()-1, loc.getBlockY()-3, loc.getBlockZ()+3).setType(Material.POLISHED_ANDESITE); // RIGHT
+		world.getBlockAt(loc.getBlockX()-1, loc.getBlockY()-3, loc.getBlockZ()+4).setType(Material.POLISHED_ANDESITE); // FRONT RIGHT
+		
+		// South Layer 3 Stairs
+		world.getBlockAt(loc.getBlockX(), loc.getBlockY()-3, loc.getBlockZ()+4).setType(Material.STONE_BRICK_STAIRS);  // Front Middle
+		block = world.getBlockAt(loc.getBlockX(), loc.getBlockY()-3, loc.getBlockZ()+4);
+		direction = (Directional) block.getBlockData();
+		direction.setFacing(BlockFace.NORTH);
+		block.setBlockData(direction);
+		
+		// West Layer 2
+		world.getBlockAt(loc.getBlockX()-3, loc.getBlockY()-2, loc.getBlockZ()+1).setType(Material.POLISHED_ANDESITE); // Left
+		world.getBlockAt(loc.getBlockX()-3, loc.getBlockY()-2, loc.getBlockZ()-1).setType(Material.POLISHED_ANDESITE); // Right
+		
+		// West Layer 2 Stairs
+		world.getBlockAt(loc.getBlockX()-3, loc.getBlockY()-2, loc.getBlockZ()).setType(Material.STONE_BRICK_STAIRS);  // Middle
+		block = world.getBlockAt(loc.getBlockX()-3, loc.getBlockY()-2, loc.getBlockZ());
+		direction = (Directional) block.getBlockData();
+		direction.setFacing(BlockFace.EAST);
+		block.setBlockData(direction);
+		
+		// West Layer 3
+		world.getBlockAt(loc.getBlockX()-3, loc.getBlockY()-3, loc.getBlockZ()).setType(Material.POLISHED_ANDESITE); // Middle
+		world.getBlockAt(loc.getBlockX()-3, loc.getBlockY()-3, loc.getBlockZ()+1).setType(Material.POLISHED_ANDESITE); // Left
+		world.getBlockAt(loc.getBlockX()-4, loc.getBlockY()-3, loc.getBlockZ()+1).setType(Material.POLISHED_ANDESITE); // Front Left
+		world.getBlockAt(loc.getBlockX()-3, loc.getBlockY()-3, loc.getBlockZ()-1).setType(Material.POLISHED_ANDESITE); // Right
+		world.getBlockAt(loc.getBlockX()-4, loc.getBlockY()-3, loc.getBlockZ()-1).setType(Material.POLISHED_ANDESITE); // Front Right
+		
+		// West Layer 3 Stairs
+		world.getBlockAt(loc.getBlockX()-4, loc.getBlockY()-3, loc.getBlockZ()).setType(Material.STONE_BRICK_STAIRS);  // Front Middle
+		block = world.getBlockAt(loc.getBlockX()-4, loc.getBlockY()-3, loc.getBlockZ());
+		direction = (Directional) block.getBlockData();
+		direction.setFacing(BlockFace.EAST);
+		block.setBlockData(direction);
+		
+		// East Layer 2
+		world.getBlockAt(loc.getBlockX()+3, loc.getBlockY()-2, loc.getBlockZ()+1).setType(Material.POLISHED_ANDESITE); // Left
+		world.getBlockAt(loc.getBlockX()+3, loc.getBlockY()-2, loc.getBlockZ()-1).setType(Material.POLISHED_ANDESITE); // Right
+		
+		// East Layer 2 Stairs
+		world.getBlockAt(loc.getBlockX()+3, loc.getBlockY()-2, loc.getBlockZ()).setType(Material.STONE_BRICK_STAIRS);  // Front Middle
+		block = world.getBlockAt(loc.getBlockX()+3, loc.getBlockY()-2, loc.getBlockZ());
+		direction = (Directional) block.getBlockData();
+		direction.setFacing(BlockFace.WEST);
+		block.setBlockData(direction);
+		
+		// East Layer 3
+		world.getBlockAt(loc.getBlockX()+3, loc.getBlockY()-3, loc.getBlockZ()).setType(Material.POLISHED_ANDESITE); // Middle
+		world.getBlockAt(loc.getBlockX()+3, loc.getBlockY()-3, loc.getBlockZ()-1).setType(Material.POLISHED_ANDESITE); // Left
+		world.getBlockAt(loc.getBlockX()+4, loc.getBlockY()-3, loc.getBlockZ()-1).setType(Material.POLISHED_ANDESITE); // Front Left
+		world.getBlockAt(loc.getBlockX()+3, loc.getBlockY()-3, loc.getBlockZ()+1).setType(Material.POLISHED_ANDESITE); // Right
+		world.getBlockAt(loc.getBlockX()+4, loc.getBlockY()-3, loc.getBlockZ()+1).setType(Material.POLISHED_ANDESITE); // Front Right
+		
+		// East Layer 3 Stairs
+		world.getBlockAt(loc.getBlockX()+4, loc.getBlockY()-3, loc.getBlockZ()).setType(Material.STONE_BRICK_STAIRS);  // Front Middle
+		block = world.getBlockAt(loc.getBlockX()+4, loc.getBlockY()-3, loc.getBlockZ());
+		direction = (Directional) block.getBlockData();
+		direction.setFacing(BlockFace.WEST);
+		block.setBlockData(direction);
+		
 	}
 }
